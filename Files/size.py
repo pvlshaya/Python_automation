67d7f8b0d915a035f11fc423caf320245ac6f9b4
pvlshaya <pavlesajinovic@gmail.com>
2024-05-04 21:19:54 +0200

    Add existing files in a new subfolder

---
+++ Files/size.py
@@ -0,0 +1,8 @@
+import os
+
+totalSize = 0
+for filename in os.listdir('C:\\Users\\Lenovo'):
+    if not os.path.isfile(os.path.join('C:\\Users\\Lenovo', filename)):
+        continue
+    totalSize = totalSize + os.path.getsize(os.path.join('C:\\Users\\Lenovo', filename))
+print(totalSize)
